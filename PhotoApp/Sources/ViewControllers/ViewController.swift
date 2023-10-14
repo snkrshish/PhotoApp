6d4969460b9fe88687f4c5bbe2fcc685916c8243
Nikita Shilyaev <shilyaef@iCloud.com>
2023-10-14 09:24:37 +0800

    start setting

---
+++ PhotoApp/Sources/ViewControllers/ViewController.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+class ViewController: UIViewController {
+    //MARK: - Outles
+
+
+
+
+    //MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+
+        setupHierarchy()
+        setupLayout()
+    }
+
+
+
+    //MARK: - Setup
+    func setupHierarchy() {
+
+    }
+
+    func setupLayout() {
+
+    }
+}
+
